e18cd5f023d1de5a7cef79318b20619550d49d90
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-02-23 17:27:16 -0600

    coded the other constructors

---
+++ engineer.js
@@ -0,0 +1,16 @@
+//extension of the employee constructor
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, id, email);
+
+        this.github = github;
+    }
+
+    getGithub() {
+        return this.github;
+    }
+
+    getRole() {
+        return "Engineer";
+    }
+}
