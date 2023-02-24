8788e615fc276fb7dc0878b6a5b200da6e0b5f6f
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-02-24 15:43:01 -0600

    added const in index for employees

---
+++ index.js
@@ -0,0 +1,7 @@
+//Connecting the lib js files
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+//connecting installed npm
+const inquirer = require('inquirer');
+const fs = require("fs");
