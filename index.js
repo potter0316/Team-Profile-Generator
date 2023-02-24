f0d8d5dcb33760470094e84eb33219f2d916c81c
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-02-24 15:52:54 -0600

    begining prompts

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 //Connecting the lib js files
+const Employee = require('./lib/employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 //connecting installed npm
 const inquirer = require('inquirer');
 const fs = require("fs");
+const generatePage = require
+
+//node prompts
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        }
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Whate is your ID number?'
+        }
+    ])
+}
