00d10e99fb173773a1ecace8dbaad619a797ff40
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-02-24 18:45:24 -0600

    added more questions and data for employee types

--- index.js
+++ index.js
@@ -42,7 +42,13 @@ const promptUser = () => {
 
 const addEmployee = () => {
     return inquirer.prompt ([
-    {
+        {
+            type: 'list',
+            name: 'role',
+            message: "What is the role of the employee?",
+            choices: ['Engineer', 'Intern']
+        },
+        {
             type: 'input',
             name: 'name',
             message: "Name of the employee?",
@@ -57,13 +63,41 @@ const addEmployee = () => {
             name: 'email',
             message: "What is your employee's email?",
         },
+        { 
+            type: 'input',
+            name: 'github',
+            message: "please enter your employee's github username",
+        },
         {
-            type: 'list',
-            name: 'role',
-            message: "What is the role of the employee?",
-            choices: ['Engineer', 'Intern']
-        }
+            type: 'input',
+            name: 'school',
+            message: 'What school was the intern a part of?',
+        },
+        .then(teamData => {
+            // data for employee types 
+            if (teamData.role === "Engineer") {
+                const  { name, id, email, github } = teamData; 
+                const engineer = new Engineer (name, id, email, github);
     
+                teamMembers.push(engineer); 
+                console.log(engineer);
+    
+            } else {
+                const  { name, id, email, school } = teamData; 
+                const intern = new Intern (name, id, email, school);
+    
+                teamMembers.push(intern);
+                console.log(intern);
+    
+        }
+            if (teamData.confirmAddEmployee) {
+                return addEmployee(teamMembers); 
+            } else {
+                return teamMembers; 
+            }
+        })
+    
+      
 ])
 }
 
